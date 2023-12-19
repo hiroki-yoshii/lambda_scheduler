431c46595865958e21ddca46614e446ad365a06b
hiroki-yoshii <2023.hkyo@gmail.com>
2023-12-20 02:13:35 +0900

    開発環境プロバイダ設定更新

--- infrastructures/dev/provider.tf
+++ infrastructures/dev/provider.tf
@@ -1,7 +1,8 @@
 terraform {
-    // tfstate(Terraformの状態を管理するファイル)をS3で管理する
+  provider "aws" {}
     backend "s3" {
     bucket = var.state_s3_bucket
     key    = var.state_key
+    region = var.aws_region 
     }
 }
